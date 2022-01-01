5c71ea4ad9875f394be2679fef4bfd52d1642ce5
Alistair Smith <alistairsmith01@gmail.com>
2022-01-01 23:13:05 +0000

    feat: impl for add

---
+++ operations/add.ts
@@ -0,0 +1,11 @@
+// Playground link:
+// https://www.typescriptlang.org/play?#code/C4TwDgpgBAKgrmANhA8gMwDIQHYHNgAWAPDADRQZQQAewOAJgM5TZwC2ARhAE7kBKVWg2YwA2gF0oAXigSAfNKh9RAImR5CKyTTrYmFAFABIAPxLjALlgJk6LBuJkK5UQDp3fcjHFyA3AYNQSCgAQ3p6IhDBXX1WTh5yDmjhFnYubgUZUWN3V3gkVEwcfGIABnIQuVIc93zbIocicqgOOQNxVXUSrX9A8GhuCEY4RGBFMIiARmaAZj8DIA
+
+type TupleOfLength<T, L extends number, R extends T[] = []> = R["length"] extends L
+	? R
+	: TupleOfLength<T, L, [...R, T]>;
+
+export type add<a extends number, b extends number> = [
+	...TupleOfLength<0, a>,
+	...TupleOfLength<0, b>
+]["length"];
